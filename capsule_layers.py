c3a71b91527f9e595c38581dcfd6cc60018d8111
ericatsu <ekaatsu@st.ug.edu.gh>
2024-05-06 20:42:01 +0000

    🐛 call

--- capsule_layers.py
+++ capsule_layers.py
@@ -19,20 +19,26 @@ class CapsuleLayer(tf.keras.layers.Layer):
                                       trainable=True)
 
     def call(self, inputs):
-        # Expand the input tensor and compute predictions
+        if len(inputs.shape) != 4:
+            raise ValueError(f"Expected inputs to CapsuleLayer to be 4D, received shape {inputs.shape}")
+
         inputs_expand = tf.expand_dims(inputs, 1)
-        inputs_tiled = tf.tile(inputs_expand, [1, self.num_capsule, 1])
+        print("After expand_dims:", inputs_expand.shape)
+
+        inputs_tiled = tf.tile(inputs_expand, [1, self.num_capsule, 1, 1, 1])
+        print("After tiling:", inputs_tiled.shape)
+
         inputs_transformed = tf.reshape(tf.matmul(inputs_tiled, self.kernel), [-1, self.num_capsule, self.dim_capsule])
+        print("After transformation:", inputs_transformed.shape)
 
-        # Initialize raw routing weights with zeros
         b = tf.zeros(shape=[tf.shape(inputs_transformed)[0], self.num_capsule])
-
         for i in range(self.routings):
             c = tf.nn.softmax(b, axis=1)
             s = tf.reduce_sum(c[:, :, None] * inputs_transformed, axis=1)
             v = squash(s)
+            print(f"Output v after routing {i+1}:", v.shape)
             if i < self.routings - 1:
-                b += tf.matmul(inputs_transformed, v[:, None, :], transpose_a=True)
+                b += tf.matmul(inputs_transformed, v[:, None, :], transpose_b=True)
 
         return v
 
@@ -52,5 +58,3 @@ def margin_loss(y_true, y_pred):
     L = y_true * tf.square(tf.maximum(0., 0.9 - y_pred)) + \
         0.5 * (1 - y_true) * tf.square(tf.maximum(0., y_pred - 0.1))
     return tf.reduce_mean(tf.reduce_sum(L, axis=1))
-
-
